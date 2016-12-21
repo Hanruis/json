69e23358ddf4e4709e0944743b6fd901e1c73445
Hanruis <183631698@qq.com>
2016-12-21 10:58:42 +0800

    json serialize 基本完成

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const Lexer = require('./Lexer');
 const AST = require('./AST');
 const ASTCompiler = require('./ASTCompiler');
-
+const _ = require('lodash');
 
 function Parser() {
     this.lexer = new Lexer();
@@ -15,5 +15,43 @@ Parser.prototype.parse = function (text) {
 };
 
 
+function serialize(obj) {
+    if (!_.isObject(obj) && !_.isArray(obj)) {
+        throw new Error();
+    }
+
+    function _serialize(src) {
+        if (_.isFunction(src)) {
+            throw new Error('serizlize target must be obj or array');
+        }
+
+        if (_.isArray(src)) {
+            const elements = _.map(src, function (item) {
+                return _serialize(item);
+            }).join(',');
+            return `[${elements}]`;
+        } else if (_.isObject(src)) {
+            const properties = _.map(src, function (value, key) {
+                const valString = _serialize(value);
+                return `"${key}":${valString}`;
+            }).join(',');
+            return `{${properties}}`;
+        } else if (_.isNumber(src)) {
+            return src;
+        } else if (_.isString(src)) {
+            return `"${src}"`;
+        } else if (_.isNull(src)) {
+            return 'null';
+        } else if (_.isBoolean(src)) {
+            return src ? 'true' : 'false';
+        } else {
+            throw new Error('unexpected serilize target:' + src);
+        }
+    }
+
+    return _serialize(obj);
+}
+
+
 exports.Parser = Parser;
-exports.toString = toString;
+exports.serialize = serialize;
--- test/index_test.js
+++ test/index_test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { Parser } = require('../src');
+const { Parser, serialize } = require('../src');
 
 describe('parse', () => {
     let parser;
@@ -80,6 +80,47 @@ describe('parse', () => {
     it('should parse object in array', () => {
         const result = parser.parse('[{"key":1}]');
         expect(result).to.eql([{ key:1 }]);
+    });
+});
+
+
+describe('serialize', () => {
+    it('should empty array', () => {
+        expect(serialize([])).to.equal('[]');
+    });
+    it('should empty obj', () => {
+        expect(serialize({})).to.equal('{}');
+    });
+    it('should literal array', () => {
+        expect(serialize([1, '123', true, null])).to.equal('[1,"123",true,null]');
+    });
+    it('should literal obj', () => {
+        expect(serialize({
+            keya:null,
+            keyb:true,
+            keyc:1,
+            keyd:'test'
+        })).to.equal('{"keya":null,"keyb":true,"keyc":1,"keyd":"test"}');
+    });
+    it('should serialize nested obj', () => {
+        expect(serialize({
+            key:{
+                a:'1'
+            }
+        })).to.equal('{"key":{"a":"1"}}');
+    });
+    it('should serialize nested obj array ', () => {
+        expect(serialize({
+            key:[1, 'a']
+        })).to.equal('{"key":[1,"a"]}');
+    });
+    it('should serialize nested array ', () => {
+        expect(serialize([[1, 'a'], [1, 'b']])).to.equal('[[1,"a"],[1,"b"]]');
+    });
+    it('should serialize nested array ', () => {
+        expect(serialize([{
+            key:1
+        }])).to.equal('[{"key":1}]');
     });
 });
 
